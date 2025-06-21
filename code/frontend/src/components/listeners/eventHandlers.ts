35c57d17d46e3be58f04d8fbc77cd3baf32188fd
Jospin <jospin.muhanuzi@gmail.com>
2025-06-21 16:52:49 -0500

    Started database configuration

---
+++ code/frontend/src/components/listeners/eventHandlers.ts
@@ -0,0 +1,5 @@
+export function submitForm(form: any) { // this can be a wrapper that returns the appropriate handler function
+    return ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        // this is where you can retrieve form input
+    });
+}
