b2c6f0ef156d24dc5737793f5d8274dae3eef530
Jospin <jospin.muhanuzi@gmail.com>
2025-06-26 02:15:02 -0500

    Removed log message from errror handler

--- code/backend/src/services/authService.ts
+++ code/backend/src/services/authService.ts
@@ -1,6 +1,4 @@
 export function handleError(req: any, res: any) {
-    console.log(req.error.status);
-    
     switch(req.error.status) {
         case 400:
             res.json({ error: "Incorrect email or password." });
