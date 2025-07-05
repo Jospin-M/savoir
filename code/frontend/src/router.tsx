e4d2e7bfa1c3158fb73ede71b3a0dd9e0f1e7bba
Jospin <jospin.muhanuzi@gmail.com>
2025-07-05 03:13:56 +0200

    Verification routes are now protected against back navigation

--- code/frontend/src/router.tsx
+++ code/frontend/src/router.tsx
@@ -4,7 +4,6 @@ import SignUp from "./pages/Auth/SignUp.tsx";
 import Login from "./pages/Auth/Login.tsx";
 import VerifyAccount from "./pages/Auth/VerifyAccount.tsx";
 import Background from "./components/auth/Background.tsx";
-import { PrivateRoute } from "./components/PrivateRoute.tsx";
 import ChangePassword from "./pages/Auth/ChangePassword.tsx";
 import InitiateReset from "./pages/Auth/InitiatePasswordReset.tsx";
 
@@ -12,17 +11,7 @@ export const router = createBrowserRouter([
     { path: "/", element: <Background/> }, // will be replaced with home page
     { path: "/auth/signup", element: <SignUp /> },
     { path: "/auth/login", element: <Login/> },
-    { path: "/auth/verifyRegistration", 
-      element: 
-        <PrivateRoute>
-            <VerifyAccount /> 
-        </PrivateRoute>
-    },
+    { path: "/auth/verifyRegistration", element: <VerifyAccount /> },
     { path: "/auth/password/sendResetLink", element: <InitiateReset/> },
-    { path: "/auth/password/reset",
-      element:
-        <PrivateRoute>
-          <ChangePassword />
-        </PrivateRoute>
-    }
+    { path: "/auth/password/reset", element: <ChangePassword /> }
 ]);
