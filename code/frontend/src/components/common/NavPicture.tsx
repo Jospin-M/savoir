f8a076f98e03b121596767461a127498cb2226b9
Jospin <jospin.muhanuzi@gmail.com>
2025-07-08 16:53:24 +0200

    Integrated loader so that api calls for user data are no longer made inside function components

---
+++ code/frontend/src/components/common/NavPicture.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+import styles from "./Common.module.css";
+
+type ImageProps = {
+    imageUrl: string;
+    cssClass: string;
+    destination: string;
+    isActiveStyle: object;
+}
+
+export default function NavPicture({ imageUrl, cssClass, destination, isActiveStyle }: ImageProps) {
+    return (
+        <NavLink 
+            to={destination} 
+            style={({ isActive }) => isActive ? isActiveStyle: {}}
+        >
+            <img className={styles[cssClass]} src={imageUrl}/>
+        </NavLink>
+    );
+}
