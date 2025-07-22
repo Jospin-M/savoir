4f4188b4fd3032a53313a0e6c5ac152810a639ee
Jospin <jospin.muhanuzi@gmail.com>
2025-07-22 11:36:25 +0200

    Auth background is now responsive

--- code/frontend/src/components/auth/Background.tsx
+++ code/frontend/src/components/auth/Background.tsx
@@ -1,15 +1,15 @@
 import { useEffect, type ReactNode } from "react";
 
+import style from "./Auth.module.css";
+
 type BackgroundProps = {
     color?: string;
     children?: ReactNode;
 };
 
-export default function Background({ color = "#EC9A29", children }: BackgroundProps) {
-    document.body.style.display = "flex";
-    document.body.style.justifyContent = "center";
-    document.body.style.alignItems = "center";
-    
+export default function Background({ children }: BackgroundProps) {
+    const color = "#EC9A29";
+
     useEffect(() => {
         const original = document.body.style.backgroundColor;
         document.body.style.backgroundColor = color;
@@ -20,7 +20,7 @@ export default function Background({ color = "#EC9A29", children }: BackgroundPr
     }, [color]);
 
     return (
-        <div>
+        <div className={style.auth_background}>
             {children}
         </div>
     );
