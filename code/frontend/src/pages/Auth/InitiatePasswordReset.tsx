0bf37b9fdf318126d1fb9cec079668ac6b472e1a
Jospin Muhanuzi <jospin.muhanuzi@gmail.com>
2025-08-04 14:22:15 -0500

    Adjusted padding and color of page backgrounds

--- code/frontend/src/pages/Auth/InitiatePasswordReset.tsx
+++ code/frontend/src/pages/Auth/InitiatePasswordReset.tsx
@@ -37,34 +37,36 @@ export default function InitiateReset() {
     }
 
     return (
-       <Rectangle cssClass="verification_password_page_background">
-            <div className={styles.initiate_reset_container}>
-                <h1 className={styles.box_header}>Verify Email</h1>
+       <div className={styles.auth_background}>
+            <Rectangle cssClass="verification_password_page_background">
+                <div className={styles.initiate_reset_container}>
+                    <h1 className={styles.box_header}>Verify Email</h1>
 
-                <p className={styles.password_verification_prompt_text}>
-                    Enter your email address and we'll send you a link that you can use to reset your password.
-                </p>
+                    <p className={styles.password_verification_prompt_text}>
+                        Enter your email address and we'll send you a link that you can use to reset your password.
+                    </p>
 
-                <div className={styles.reset_input_container}>
-                    <form>
-                        <InputBox input_box_title="Email" type="email" name="email" handleChange={saveInput} />
-                    </form>
+                    <div className={styles.reset_input_container}>
+                        <form>
+                            <InputBox input_box_title="Email" type="email" name="email" handleChange={saveInput} />
+                        </form>
 
-                    <div className={styles.verification_status_container}>
-                        {messageToDisplay && <p className={styles[styleToUse]}>{messageToDisplay}</p>}
+                        <div className={styles.verification_status_container}>
+                            {messageToDisplay && <p className={styles[styleToUse]}>{messageToDisplay}</p>}
+                        </div>
+                    </div>
+
+                    <div className={styles.initiate_reset_navigation_buttons_container }>
+                        <div className={styles.nav_button_container}>
+                            <Button prompt="Back" isDisabled={false} handleClick={()=>navigate("/auth/login")}/>
+                        </div>
+                        
+                        <div className={styles.nav_button_container}>
+                            <Button prompt="Submit" isDisabled={!validateEmail(form.email)} handleClick={handleEmailVerification}/>
+                        </div>
                     </div>
                 </div>
-
-                <div className={styles.initiate_reset_navigation_buttons_container }>
-                    <div className={styles.nav_button_container}>
-                        <Button prompt="Back" isDisabled={false} handleClick={()=>navigate("/auth/login")}/>
-                    </div>
-                    
-                    <div className={styles.nav_button_container}>
-                        <Button prompt="Submit" isDisabled={!validateEmail(form.email)} handleClick={handleEmailVerification}/>
-                    </div>
-                </div>
-            </div>
-        </Rectangle>
+            </Rectangle>
+       </div>
     );
 }
