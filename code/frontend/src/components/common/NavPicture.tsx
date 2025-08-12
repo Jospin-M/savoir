cc4cf36f650704204abea4128cb45348cd560eb0
Jospin <jospin.muhanuzi@gmail.com>
2025-08-12 01:18:50 -0500

    "Edit Profile" overlay is functioning correctly.
    Next steps include the following:
    - modularize EditProfileModal component into smaller functional components (keep them in the same tsx file, where approriate).
    
    - define behaviour for the buttons that are used to change cover and profile photos.
    
    - generalize the creation of language listings (both name and profiency level) to avoid repetition (use a similar approach to Sidebar.tsx).
    
    - define behaviour for the "add new language" button. (it should add a new language item to the list with English as the default language). if a language, has already been selected, it shouldn't be available as an option for the other language items.
    
    - define behaviour for the icon to delete a language item.
    
    - define a function that preps form data before it is sent to the database. this will be the onClick handler for the "save" button - this should trigger a page refresh submission
    
    - make adjustments to database schema
    
    - incorporate React Query formaking requests and Next SSR
    
    - finally, build the "containers" for user skills and review

--- code/frontend/src/components/common/NavPicture.tsx
+++ code/frontend/src/components/common/NavPicture.tsx
@@ -5,7 +5,7 @@ import Link from "next/Link";
 export default function NavPicture() {
     // ratio of images must be changed to square before being saved in database
     // href will use the id passed in through ssr
-    console.log()
+    
     return (
         <Link href={"/profile/" + ""} className={styles.profile_pic_link}>
             <img className={styles.nav_bar_profile_pic} src={"https://static01.nyt.com/images/2020/03/09/sports/09nba-topteams1/09nba-topteams1-mediumSquareAt3X.jpg"}/>
--- code/frontend/src/components/profile/Sidebar.tsx
+++ code/frontend/src/components/profile/Sidebar.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import type { JSX } from "react";
+import EditProfileModal from "./EditProfileModal";
+import Button from "../common/Button";
 
 import styles from "./Profile.module.css";
+
+import { useState } from "react";
 
 type Language = {
     name: string,
@@ -48,9 +54,8 @@ function createLanguageItems(languages: Language[]) {
 }
 
 export default function Sidebar() {
-    // max character count for the bio (including spaces) should be 230 - reinforce this on the database
-    // editing the profile in any way refresh the page so 
-    // that updated data is shown when the user completes the action
+    // editing the profile in any way should refresh the page so that updated data
+    // is shown when the user completes the action
     const languages: Language[] = [ // these would be retrieved from the database
         { name: "English", proficiency: "advanced" },
         { name: "French", proficiency: "advanced" },
@@ -59,33 +64,36 @@ export default function Sidebar() {
     ];
     const languageItems = createLanguageItems(languages);
 
-    // create modal overlay to edit (cover photo, name, profile pic,
-    // bio, and languages)
-    // also, don't forget to add button to edit profile 
+    const [showModal, setShowModal] = useState(false);
+
     return ((
-        <div className={styles.profile_sidebar}>
-            <div className={styles.card} id={styles.profile}>
-                <div className={styles.card_header}>
-                    <h2>About Me</h2>
+        <div className={styles.sidebar_container}>
+            <div className={styles.profile_sidebar}>
+                <div className={styles.card} id={styles.profile}>
+                    <div className={styles.card_header}>
+                        <h2>About Me</h2>
+                    </div>
 
-                    <i className={"ri-edit-line"}/>
+                    <p className={styles.bio_text}>
+                        Passionate language teacher with 5+ years of experience. I love helping others discover new cultures through language learning. In my free time, I enjoy hiking and photography and exploring local cuisine with friends from Italy.    
+                    </p>
                 </div>
 
-                <p className={styles.bio_text}>
-                    Passionate language teacher with 5+ years of experience. I love helping others discover new cultures through language learning. In my free time, I enjoy hiking and photography and exploring local cuisine with friends from Italy.    
-                </p>
-            </div>
+                <div className={styles.card}>
+                    <div className={styles.card_header}>
+                        <h2>Languages</h2>
+                    </div>
 
-            <div className={styles.card}>
-                <div className={styles.card_header}>
-                    <h2>Languages</h2>
-
-                    <i className={"ri-edit-line"}/>
+                    <div className={styles.languages_container}>
+                        {languageItems}
+                    </div>
                 </div>
 
-                <div className={styles.languages_container}>
-                    {languageItems}
+                <div className={styles.edit_profile_button_container}>
+                    <Button prompt="Edit Profile" isDisabled={false} handleClick={() => setShowModal(true)}/>
                 </div>
+
+                {showModal && <EditProfileModal closeButtonHandler={() => setShowModal(false)} />}
             </div>
         </div>
     ));
