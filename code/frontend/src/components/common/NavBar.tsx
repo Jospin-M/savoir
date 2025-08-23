1e8a95adfafd746dab0a63840b3dece637e8a0c1
Jospin <jospin.muhanuzi@gmail.com>
2025-08-23 01:30:21 -0500

    Behaviour has been defined to style navigation items when they're in their "active" state.

--- code/frontend/src/components/common/NavBar.tsx
+++ code/frontend/src/components/common/NavBar.tsx
@@ -1,8 +1,20 @@
-import Link from "next/link";
-
 import styles from "./Common.module.css";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+/**
+ * Determines if the active style for the Link should be shown depending on the URL parameters and 
+ * returns the appropriate styling.
+ */
+function getStyle(currentPathName: string | null, pathnameToCheck: string) {
+    return currentPathName === pathnameToCheck ? 
+    `${styles.nav_item}  ${styles.active}`: styles.nav_item;
+}
+
 export default function NavBar() {
+    const pathname = usePathname();
+    
     return (
         <div className={styles.sidebar}>
             <Link href="" className={styles.nav_item}>
@@ -10,7 +22,7 @@ export default function NavBar() {
                 <div className={styles.nav_item_text}>Dashboard</div>
             </Link>
 
-            <Link href="/skills" className={styles.nav_item}>
+            <Link href="/skills" className={getStyle(pathname, "/skills")}>
                 <i className="ri-lightbulb-line" id={styles.nav_item_logo} />
                 <div className={styles.nav_item_text}>Skills</div>
             </Link>
