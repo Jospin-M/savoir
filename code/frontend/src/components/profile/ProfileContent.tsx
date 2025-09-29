d83a3a956a507acce53c22f3f56564ede79a5866
Jospin <jospin.muhanuzi@gmail.com>
2025-09-28 20:15:51 -0600

    The following bugs have been addressed: - regardless of how small the screen gets, the skill cards have a minimum set width - now both profile and skill pages use the .page_content class and an id is used to provide more specific styling for the skills page - the length of the header is now responsive to the screensize and its contained components are fully responsive - nav bar width is also now responsive and has a consistent width across pages

--- code/frontend/src/components/profile/ProfileContent.tsx
+++ code/frontend/src/components/profile/ProfileContent.tsx
@@ -1,11 +1,18 @@
 import Sidebar from "./sidebar/Sidebar";
+import ProfileHeader from "./ProfileHeader";
 
+import commonStyles from "../common/Common.module.css"
 import styles from "./Profile.module.css";
 
 export default function ProfileContent() {
     return (
-        <div className={styles.profile_content}>
+        <div className={commonStyles.page_content}>
+            <ProfileHeader />
+
             <Sidebar />
+            <div className={styles.profile_content}>
+                
+            </div>
         </div>
     );
 }
