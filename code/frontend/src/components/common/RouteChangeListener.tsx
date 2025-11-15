827186585315f57f76335305f14df5b8fbee19be
Jospin <jospin.muhanuzi@gmail.com>
2025-11-14 22:57:34 -0700

    Fixed a bug that was being caused by rendering data in a setState call.

--- code/frontend/src/components/common/RouteChangeListener.tsx
+++ code/frontend/src/components/common/RouteChangeListener.tsx
@@ -8,7 +8,7 @@ import { type UserProfile, useUserStore } from "../../stores/useUserStore";
 
 export function RouteChangeListener() {
     const pathname = usePathname();
-    const [, setPrevPath] = useState(""); // useState is used here so we can obtain the previous path
+    const [prevPath, setPrevPath] = useState(""); // useState is used here so we can obtain the previous path
     
     const userID = useUserStore(state => state.userID);
     const userProfile = useUserStore(state => state.userProfile);
@@ -23,17 +23,18 @@ export function RouteChangeListener() {
     const { refresh: updateProfile } = useRefreshCache<UserProfile>("/profiles/me", "PUT", { key: "user-profile", param: userID! });
     
     useEffect(() => {
-        setPrevPath((prev) => {
-            if(prev === `/profile/${userID}` && isProfileUpdated && userProfile) {
-                updateProfile(userProfile);
-                setIsProfileUpdated(false);
-            } else if(prev === "/skills" && isSkillsCacheUpdated && skills) {
-                updateSkills(skills);
-                setIsSkillsUpdated(false);
-            }
+        setPrevPath(pathname);
+    }, [pathname]);
 
-            return pathname ? pathname: "";
-        }); 
+    useEffect(() => {
+        if(prevPath === `/profile/${userID}` && isProfileUpdated && userProfile) {
+            updateProfile(userProfile);
+            setIsProfileUpdated(false);
+        } else if(prevPath === "/skills" && isSkillsCacheUpdated && skills) {
+            updateSkills(skills);
+            setIsSkillsUpdated(false);
+        }
+        
 
         return () => {}; // cleanup function is required here since not using one causes the Effect to be run twice
     }, [pathname]); // the reason the other dependencies are not added is because they cause the effect to run on every change, which is not what we want 
